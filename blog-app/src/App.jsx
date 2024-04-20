17557f7a5cae36aa1fbba17dba1dbce51497c00d
Wisdom Okogho <wisdommokoghoo@gmail.com>
2024-04-20 16:25:33 +0100

    Create pages and Routes

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
 export default function App() {
-  return <div className="bg-red-300">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
